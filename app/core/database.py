1c5c9fb262f424f02d7b7fd0f0d7d0fb85b6f4b3
jigneshsapra1990 <jignesh.sapra@gmail.com>
2026-05-24 22:56:09 +0530

    Refactor database setup to use asynchronous SQLAlchemy engine and session management

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
